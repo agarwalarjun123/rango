f46470168d6179a8d2b0b13b6196038dbc9411ed
arjun Agarwal <arjunagarwal1998@gmail.com>
2022-01-28 21:24:34 +0000

    chapter 6 completed

--- populate_rango.py
+++ populate_rango.py
@@ -7,11 +7,6 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
- # First, we will create lists of dictionaries containing the pages
- # we want to add into each category.
- # Then we will create a dictionary of dictionaries for our categories.
- # This might seem a little bit confusing, but it allows us to iterate
- # through each data structure, and add the data to our models.
 
  python_pages = [
  {'title': 'Official Python Tutorial',
@@ -39,20 +34,15 @@ def populate():
  'Django': {'pages': django_pages,'views': 64,'likes': 32 },
  'Other Frameworks': {'pages': other_pages, 'views': 32 ,'likes': 16} }
 
- # If you want to add more categories or pages,
- # add them to the dictionaries above.
-
- # The code below goes through the cats dictionary, then adds each category,
- # and then adds all the associated pages for that category.
  for cat, cat_data in cats.items():
     c = add_cat(cat,cat_data)
     for p in cat_data['pages']:
         add_page(c, p['title'], p['url'],p['views'])
 
- # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
+
 
 def add_page(cat, title, url, views=0):
  p = Page.objects.get_or_create(category=cat, title=title)[0]
